fc4c2eac7a4c92c60c9ab3f25bccf24ddf5091df
moturimanideep <manideep.digischool@gmail.com>
2018-07-06 16:34:35 +0530

    first commiy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import {MatCardModule, MatButtonModule} from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent, HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatCardModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
